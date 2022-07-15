cb9a23ab239905adb206eeb669dd9669f7810221
Sergey Manshin <vvintpann@gmail.com>
2022-07-15 14:44:53 +0300

    fast commit

---
+++ redhat/fonts.sh
@@ -0,0 +1,34 @@
+sudo tee -a ~/Desktop/local.conf <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+  <match target="font">
+    <edit name="antialias" mode="assign">
+      <bool>true</bool>
+    </edit>
+    <edit name="hinting" mode="assign">
+      <bool>true</bool>
+    </edit>
+    <edit mode="assign" name="rgba">
+      <const>rgb</const>
+    </edit>
+    <edit mode="assign" name="hintstyle">
+      <const>hintslight</const>
+    </edit>
+    <edit mode="assign" name="lcdfilter">
+      <const>lcddefault</const>
+    </edit>
+  </match>
+</fontconfig>
+EOF
+
+sudo tee -a ~/.Xresources <<EOF
+Xft.antialias: 1
+Xft.hinting: 1
+Xft.rgba: rgb
+Xft.hintstyle: hintslight
+Xft.lcdfilter: lcddefault
+EOF
+
+
+xrdb -merge ~/.Xresources
